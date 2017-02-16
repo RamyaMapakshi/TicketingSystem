09a561e7c2977683d6c0fad4720e2ed5f17773ff
Vainktesh Kumar <v8080@live.in>
2017-02-16 16:56:36 +0530

    Removed unnecessory lines.

--- TicketingSystem.Console/Program.cs
+++ TicketingSystem.Console/Program.cs
@@ -14,25 +14,6 @@ namespace TicketingSystem.Console
             DBManager db = new DBManager();
             
             var y=db.GetAllTickets();
-            foreach (var item in y)
-            {
-                item.DueDate = DateTime.Now;
-                db.UpsertTicketObject(item);
-            }
-            var user = db.GetUserById(1);
-            //db.SaveComment(new DB.ViewModel.Comment()
-            //{
-            //    ID = 0,
-            //    Created = DateTime.Now,
-            //    CreatedBy = user,
-            //    Details = "sdsfsfdsd",
-            //    IsPrivate = false,
-            //    Modified = DateTime.Now,
-            //    ModifiedBy = user,
-            //    TicketId = new DB.ViewModel.Ticket(),
-            //    Attachment=null
-            //});
-
         }
     }
 }
