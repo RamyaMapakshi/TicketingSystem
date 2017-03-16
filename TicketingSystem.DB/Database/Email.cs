6f589053cbdaccf2da3eb19ceb9209abd19cef8d
vainktesh <vainktesh@VENUS>
2017-03-17 01:11:01 +0530

    .

---
+++ TicketingSystem.DB/Database/Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketingSystem.DB.Database
+{
+    [Table("Email")]
+    public partial class Email
+    {
+        public int ID { get; set; }
+        public string From { get; set; }
+        public List<string> CC { get; set; }
+        public List<string> To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public int? TicketID { get; set; }
+        public int? PreviousEmail { get; set; }
+        public int? AttachmentId { get; set; }
+        public DateTime DateTimeCreated { get; set; }
+        public DateTime DateTimeReceived { get; set; }
+        public DateTime DateTimeSent { get; set; }
+    }
+}
---
+++ TicketingSystem.Service/Controllers/EmailController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TicketingSystem.DB;
+
+namespace TicketingSystem.Service.Controllers
+{
+    public class EmailController : ApiController
+    {
+
+        IDBManager dbManager;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dbManager"></param>
+        public EmailController(IDBManager dbManager)
+        {
+            this.dbManager = dbManager;
+        }
+
+        [Route("api/Email/ParseAndInsertEmail/")]
+        public int ParseAndInsertEmail()
+        {
+            return 0;
+        }
+    }
+}
